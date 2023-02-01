52d633bf8c33e9220d215a8792cc54b8ff4d6d4e
Kelle-Neal <kelle.neal@student.awesomeinc.org>
2023-02-01 22:04:01 +0000

    playing around

--- js/main.js
+++ js/main.js
@@ -1,18 +1,44 @@
-const largeImgBox = document.querySelector('.full-img');
-
-const darkLightButton = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
-
+const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
 
-//*Image array *//
-const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+/* Declaring the array of image filenames */
+
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 const alts = {
-    'pic1.jpg' : 'Closeup of human eye.',
-    'pic2.jpg' : 'Rock with wave pattern',
-    'pic3.jpg' : 'Purple and white pansies beginning to wilt.',
-    'pic4.jpg' : 'Egyptian drawings and hieroglyphs.',
-    'pic5.jpg' : 'Brown cruiser butterfly on leaf.',
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'Rock that looks like a wave',
+  'pic3.jpg' : 'Purple and white pansies',
+  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+  'pic5.jpg' : 'Large moth on a leaf'
 }
 
+/* Looping through images */
+
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
